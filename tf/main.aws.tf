45c094dab06dcff393658259e988ca6d06c825bc
kukulam <kukula.milosz@gmail.com>
2023-09-02 14:11:25 +0200

    Create simple bucket with lifecycle rules.

--- tf/main.aws.tf
+++ tf/main.aws.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-bucket-name"
+  bucket = "kukulam-example-bucket-name"
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "example_bucket_lifecycle" {
