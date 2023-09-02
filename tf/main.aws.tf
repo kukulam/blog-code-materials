60a108d0117e019979b151d574de96250b263ff6
kukulam <kukula.milosz@gmail.com>
2023-09-02 14:51:04 +0200

    Create versioned bucket with lifecycle rules.

--- tf/main.aws.tf
+++ tf/main.aws.tf
@@ -10,21 +10,10 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "versioned_bucket_lifecycle" {
-  depends_on = [aws_s3_bucket_versioning.versioning]
-
-  bucket = aws_s3_bucket.versioned_bucket.id
-
-  rule {
-    id      = "expire-objects"
-    status  = "Enabled"
-
-#    expiration {
-#      days = 30
-#    }
-
-    noncurrent_version_expiration {
-      days = 30
-    }
-  }
-}
+#resource "aws_s3_bucket_lifecycle_configuration" "versioned_bucket_lifecycle" {
+#  depends_on = [aws_s3_bucket_versioning.versioning]
+#
+#  bucket = aws_s3_bucket.versioned_bucket.id
+#
+#
+#}
