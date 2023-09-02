b1a55adca8872025cb51176bb9c7f6a1a8d42b45
kukulam <kukula.milosz@gmail.com>
2023-09-02 13:50:08 +0200

    testing terraform cloud

--- tf/main.aws.tf
+++ tf/main.aws.tf
@@ -1,3 +1,15 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "example-bucket-name"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example_bucket_lifecycle" {
+  rule {
+    id      = "expire-objects"
+    status  = "Enabled"
+    prefix  = ""
+
+    expiration {
+      days = 30
+    }
+  }
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,14 +1,4 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
-}
-
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
-}
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+  default     = "eu-west-1"
 }
