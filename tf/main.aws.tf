4fb2a1109a62111745ecfdf79d95045412746671
kukulam <kukula.milosz@gmail.com>
2023-09-02 13:54:38 +0200

    Create simple bucket with lifecycle rules.

--- tf/main.aws.tf
+++ tf/main.aws.tf
@@ -7,7 +7,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "example_bucket_lifecycle" {
   rule {
     id      = "expire-objects"
     status  = "Enabled"
-    prefix  = ""
 
     expiration {
       days = 30
