23288b506d57dd971f5ef65a5c325bc6abc880a9
kukulam <kukula.milosz@gmail.com>
2023-12-10 14:19:16 +0100

    test gcp

--- tf/main.gcp.tf
+++ tf/main.gcp.tf
@@ -1,16 +1,16 @@
-#resource "google_storage_bucket" "my_versioned_bucket" {
-#  name          = "kukulam-gcs-versioned-bucket"
-#  location      = "US"
-#  versioning {
-#    enabled = true
-#  }
-#
-#  lifecycle_rule {
-#    condition {
-#      age  = 30
-#    }
-#    action {
-#      type = "Delete"
-#    }
-#  }
-#}
+resource "google_storage_bucket" "my_versioned_bucket" {
+  name          = "kukulam-gcs-versioned-bucket"
+  location      = "US"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age  = 30
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
