7f38a4f599f108e02e6251d745b1b115d589821d
kukulam <kukula.milosz@gmail.com>
2023-12-10 13:42:39 +0100

    test gcp

--- tf/main.gcp.tf
+++ tf/main.gcp.tf
@@ -1,18 +1,14 @@
 resource "google_storage_bucket" "my_bucket" {
   name          = "my-gcs-bucket"
   location      = "US"
-}
 
-resource "google_storage_bucket_lifecycle_rule" "expire_rule" {
-  count = 1
-
-  action {
-    type = "Delete"
+  lifecycle_rule {
+    condition {
+      age  = 7
+      matches_prefix = ["temp/"]
+    }
+    action {
+      type = "Delete"
+    }
   }
-
-  condition {
-    age = 30
-  }
-
-  depends_on = [google_storage_bucket.my_bucket]
 }
